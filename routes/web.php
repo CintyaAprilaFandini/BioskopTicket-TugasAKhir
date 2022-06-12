489d16866bf3d6745abcef5eb8b348c4cc314301
CintyaAprilaFandini <cintyaacaf07@gmail.com>
2022-06-13 00:13:48 +0700

    PROGRESS 3 - Tubes (Aida)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +19,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/log-in', [PageController::class, 'logIn'])->name('log-in');
+Route::get('/sign-up', [PageController::class, 'signUp'])->name('sign-up');
+Route::get('/tiket-saya', [UserController::class, 'tiketSaya'])->name('tiketSaya');
+Route::get('/forgot-password', [PageController::class, 'forgotPassword'])->name('forgot-password');
+Route::resource('movies', MovieController::class);
+Route::resource('users', UserController::class);
+Route::prefix('booking')->group(function() {    
+    Route::get('/index', [BookingController::class, 'index'])->name('booking.index');   
+    Route::get('/create/{film_id}', [BookingController::class, 'create'])->name('booking.create');   
+    Route::post('/store', [BookingController::class, 'store'])->name('booking.store');   
+    Route::delete('/delete/{id}', [BookingController::class, 'destroy'])->name('booking.destroy');   
+    Route::get('/edit', [BookingController::class, 'edit'])->name('booking.edit');   
+    Route::get('/print_invoice/{id}', [BookingController::class, 'print_pdf'])->name('booking.print');
+    Route::get('/payment/{id}', [BookingController::class, 'payment'])->name('booking.payment');
+});
